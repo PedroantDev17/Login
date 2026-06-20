944633cf9d05722dfebdb413a4f23f2f0d813760
Pedro Antônio <272311073+PedroantDev17@users.noreply.github.com>
2026-06-20 17:54:34 -0300

    Segurança no login

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ function enviar() {
             let campoNome = document.getElementById('inome');
             let campoEmail = document.getElementById('iemail');
             let campoSenha = document.getElementById('isenha');
-
+           
             var nome = campoNome.value.trim();
             var email = campoEmail.value.trim();
             var senha = campoSenha.value.trim();
-
-            if (nome.length === 0 || nome.length < 5 || nome.length > 50  || nome.length != Number, 
-                email.length === 0 || email.length < 10 || email.length > 256,
-                 senha.length < 6 || senha.length === 0 || senha.length > 10 || senha.length != undefined ) {
+            
+            //validação 
+            if (nome.length === 0 , email.length === 0 , senha.length === 0 ) {
                 window.alert('[Erro!] cadastro incorreto !!')
+            } 
+
+            // nome
+            else if (nome.length <5){
+                 window.alert('Nome menor de 5 caracteres')
+            } else if (nome.length > 10){
+                window.alert('Nome maximo de 10 caracteres')
+            } else if (nome.length === Number) {
+                window.alert('Só letras podem ser colocados!')
+            } 
+
+            // email 
+            else if (email.length < 10) {
+                 window.alert('Email menor de 10 caracteres')
+            }else if (email.length > 256) {
+                 window.alert('Email maior de 256 caracteres')
+             }else if (!email.includes('@')) {
+                window.alert('Email errado!')
+             }                
+            //senha
+            else if (senha.length < 6) {
+                window.alert('Senha menor de 6 caracteres')
             } else {
-                window.confirm(`Confirma esses dados? Nome: <strong>${nome}</strong> email: <strong>${email}</strong>`)
+                window.confirm(`Confirma esses dados? Nome: ${nome} email: ${email}`)
                 
-                fetch('https://commute-doormat-operator.ngrok-free.dev/cadastro', {
+                    fetch('https://commute-doormat-operator.ngrok-free.dev/cadastro', {
                     method:'POST',
                     headers: {
                         "Content-Type": 'application/json'
@@ -23,12 +44,12 @@ function enviar() {
                     body: JSON.stringify({
                         "name": nome, 
                         "email" : email,
-                        "senha" : senha
-                    })
-                })
-
+                        "senha" : senha}
+                    )
+                    }
+                )
+                }
+            
             }
-        }
 
-        
-
+       
